75080bb83f1e9440b9dc3624fea3ef23d7351a95
Maria Navarro <mariacdnavarro@gmail.com>
2025-06-06 20:34:32 -0300

    Criação ContaCorrente e ContaPoupanca

--- Menu.ts
+++ Menu.ts
@@ -1,119 +1,94 @@
-import readlinesync = require("readline-sync");
+import readlinesync = require('readline-sync');
 import { colors } from './src/util/Colors';
 import { Conta } from './src/model/Conta';
+import { ContaCorrente } from './src/model/ContaCorrente';
+import { ContaPoupanca } from './src/model/ContaPoupanca';
 
 export function main() {
-    
     let opcao: number;
 
-    const conta: Conta = new Conta(1, 123, 1, "Adriana", 1000);
-    conta.visualizar();
-    conta.sacar(1050);
-    conta.visualizar();
-    conta.depositar(5000);
-    conta.visualizar();
 
     while (true) {
-        console.log(colors.bg.black, colors.fg.magenta,
-                    "");
-        console.log("╔══════════════════════════════════════════════╗")
-        console.log("║             🏦 Banco Nova Era 🏦             ║");
-        console.log("╠══════════════════════════════════════════════╣");
-        console.log("║                                              ║");    
-        console.log("║ 1 - Criar Conta                              ║");
-        console.log("║ 2 - Listar todas as Contas                   ║");
-        console.log("║ 3 - Buscar Conta por Número                  ║");
-        console.log("║ 4 - Atualizar Dados da Conta                 ║");
-        console.log("║ 5 - Apagar Conta                             ║");
-        console.log("║ 6 - Sacar                                    ║");
-        console.log("║ 7 - Depositar                                ║");
-        console.log("║ 8 - Transferir valores entre Contas          ║");
-        console.log("║ 9 - Sair                                     ║");
-        console.log("║                                              ║");
-        console.log("╚══════════════════════════════════════════════╝", colors.reset);
+        console.log(colors.bg.black, colors.fg.magenta, '');
+        console.log('╔══════════════════════════════════════════════╗');
+        console.log('║             🏦 Banco Nova Era 🏦             ║');
+        console.log('╠══════════════════════════════════════════════╣');
+        console.log('║                                              ║');
+        console.log('║ 1 - Criar Conta                              ║');
+        console.log('║ 2 - Listar todas as Contas                   ║');
+        console.log('║ 3 - Buscar Conta por Número                  ║');
+        console.log('║ 4 - Atualizar Dados da Conta                 ║');
+        console.log('║ 5 - Apagar Conta                             ║');
+        console.log('║ 6 - Sacar                                    ║');
+        console.log('║ 7 - Depositar                                ║');
+        console.log('║ 8 - Transferir valores entre Contas          ║');
+        console.log('║ 9 - Sair                                     ║');
+        console.log('║                                              ║');
+        console.log('╚══════════════════════════════════════════════╝', colors.reset);
 
-        opcao = readlinesync.questionInt("Escolha uma opcao: ");
+        opcao = readlinesync.questionInt('Escolha uma opcao: ');
 
         if (opcao == 9) {
-            console.log(colors.fg.magentastrong,
-                "\nBanco Nova Era - O banco do futuro!");
+            console.log(colors.fg.magentastrong, '\nBanco Nova Era - O banco do futuro!');
             sobre();
-            console.log(colors.reset, "");
+            console.log(colors.reset, '');
             process.exit(0);
         }
 
         switch (opcao) {
             case 1:
-                console.log(colors.fg.whitestrong,
-                    "Criar Conta", colors.reset);
-
-                keyPress()
+                console.log(colors.fg.whitestrong, 'Criar Conta', colors.reset);
+                keyPress();
                 break;
             case 2:
-                console.log(colors.fg.whitestrong,
-                    "Listar todas as Contas", colors.reset);
-
-                keyPress()
+                console.log(colors.fg.whitestrong, 'Listar todas as Contas', colors.reset);
+                keyPress();
                 break;
             case 3:
-                console.log(colors.fg.whitestrong,
-                    "Buscar Conta por Número", colors.reset);
-
-                keyPress()
+                console.log(colors.fg.whitestrong, 'Buscar Conta por Número', colors.reset);
+                keyPress();
                 break;
             case 4:
-                console.log(colors.fg.whitestrong,
-                    "Atualizar Dados da Conta", colors.reset);
-
-                keyPress()
+                console.log(colors.fg.whitestrong, 'Atualizar Dados da Conta', colors.reset);
+                keyPress();
                 break;
             case 5:
-                console.log(colors.fg.whitestrong,
-                    "Apagar Conta", colors.reset);
-
-                keyPress()
+                console.log(colors.fg.whitestrong, 'Apagar Conta', colors.reset);
+                keyPress();
                 break;
             case 6:
-                console.log(colors.fg.whitestrong,
-                    "Sacar", colors.reset);
-
-                keyPress()
+                console.log(colors.fg.whitestrong, 'Sacar', colors.reset);
+                keyPress();
                 break;
             case 7:
-                console.log(colors.fg.whitestrong,
-                    "Depositar", colors.reset);
-
-                keyPress()
+                console.log(colors.fg.whitestrong, 'Depositar', colors.reset);
+                keyPress();
                 break;
             case 8:
-                console.log(colors.fg.whitestrong,
-                    "Transferir valores entre Contas", colors.reset);
-
-                keyPress()
+                console.log(colors.fg.whitestrong, 'Transferir valores entre Contas', colors.reset);
+                keyPress();
                 break;
             default:
-                console.log(colors.fg.whitestrong,
-                    "Opção inválida! Tente novamente.", colors.reset);
-
-                keyPress()
+                console.log(colors.fg.whitestrong, 'Opção inválida! Tente novamente.', colors.reset);
+                keyPress();
                 break;
         }
     }
 }
 
 export function sobre() {
-    console.log("\n══════════════════════════════════════════════");
-    console.log(" Banco Nova Era - O banco do futuro!          ");
-    console.log(" Desenvolvido por: Maria Navarro              ");
-    console.log(" Versão: 1.0.0                                ");
-    console.log(" Generation Brasil - mariacdnavarro@gmail.com ");
-    console.log(" github.com/marinavarroo                      ");
-    console.log("══════════════════════════════════════════════");
+    console.log('\n══════════════════════════════════════════════');
+    console.log(' Banco Nova Era - O banco do futuro!          ');
+    console.log(' Desenvolvido por: Maria Navarro              ');
+    console.log(' Versão: 1.0.0                                ');
+    console.log(' Generation Brasil - mariacdnavarro@gmail.com ');
+    console.log(' github.com/marinavarroo                      ');
+    console.log('══════════════════════════════════════════════');
 }
 
 function keyPress(): void {
-    console.log(colors.reset, "");
-    console.log("\nPressione enter para continuar...");
+    console.log(colors.reset, '');
+    console.log('\nPressione enter para continuar...');
     readlinesync.prompt();
 }
 
--- src/model/Conta.ts
+++ src/model/Conta.ts
@@ -13,87 +13,82 @@ export class Conta {
         this._saldo = saldo;
     }   
 
-    public get numero(){
-    return this._numero;
-}
+    public get numero() {
+        return this._numero;
+    };
 
-public set numero(numero: number) {
-    this._numero = numero;
-}
+    public set numero(numero: number) {
+        this._numero = numero;
+    };
 
-public get agencia(){
-    return this._agencia;
-}
+    public get agencia() {
+        return this._agencia;
+    };
 
-public set agencia(agencia: number) {
-    this._agencia = agencia;
-}
+    public set agencia(agencia: number) {
+        this._agencia = agencia;
+    };
 
-public get tipo(){
-    return this._tipo;
-}
+    public get tipo() {
+        return this._tipo;
+    };
 
-public set tipo(tipo: number) {
-    this._tipo = tipo;
-}
+    public set tipo(tipo: number) {
+        this._tipo = tipo;
+    };
 
-public get titular(){
-    return this._titular;
-}
+    public get titular() {
+        return this._titular;
+    };
 
-public set titular(titular: string) {
-    this._titular = titular;
-}
+    public set titular(titular: string) {
+        this._titular = titular;
+    };
 
-public get saldo(){
-    return this._saldo;
-}
+    public get saldo() {
+        return this._saldo;
+    };
 
-public set saldo(saldo: number) {
-    this._saldo = saldo;
-}
+    public set saldo(saldo: number) {
+        this._saldo = saldo;
+    };
 
-public sacar(valor: number): boolean {
+    public sacar(valor: number): boolean {
+        if (this._saldo < valor) {
+            console.log('Saldo insuficiente para saque.');
+            return false;
+        }
+        this._saldo = this._saldo - valor;
+        return true;
+    };
 
-    if (this._saldo < valor) {
-        console.log("Saldo insuficiente para saque.");
-        return false;
-    }
-    this._saldo = this._saldo - valor;
-    return true;
-}
-
-public depositar(valor: number): void {
+    public depositar(valor: number): void {
         this._saldo = this._saldo + valor;
-    }
+    };
 
-public visualizar(): void {
-    
-    let tipo: string = ""
+    public visualizar(): void {
+        let tipo: string = '';
 
-    switch (this._tipo) {
-        case 1:
-            tipo = "Conta Corrente";
-            break;
-        case 2:
-            tipo = "Conta Poupança";
-            break;
-        // case 3:
-        //     tipo = "Conta Salário";
-        //     break;
-    }
+        switch (this._tipo) {
+            case 1:
+                tipo = 'Conta Corrente';
+                break;
+            case 2:
+                tipo = 'Conta Poupança';
+                break;
+            // case 3:
+            //     tipo = 'Conta Salário';
+            //     break;
+        }
 
-    console.log("\n\n══════════════════════════════════════════════")
-    console.log('Dados da conta: ')
-    console.log("                                                  ")
-    console.log(`Numero da conta: ${this._numero}`);
-    console.log(`Agencia: ${this._agencia}`);
-    console.log(`Tipo: ${tipo}`);
-    console.log(`Titular: ${this._titular}`);
-    console.log(`Saldo: ${this._saldo.toFixed(2)}`);
-    console.log("                                                  ")
-}
-
-
-
+        console.log('\n\n══════════════════════════════════════════════');
+        console.log('Dados da conta: ');
+        console.log('                                                  ');
+        console.log(`Numero da conta: ${this._numero}`);
+        console.log(`Agencia: ${this._agencia}`);
+        console.log(`Tipo: ${tipo}`);
+        console.log(`Titular: ${this._titular}`);
+        console.log(`Saldo: ${this._saldo.toFixed(2)}`);
+        console.log('                                                  ');
+    };
 }
